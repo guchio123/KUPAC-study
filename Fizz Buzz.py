20aff13199781cd3093d6c8d807682100837e006
Noguchi <nogutaku1218@gmail.com>
2026-05-17 16:04:53 +0900

    Fizz Buzz_update

--- Fizz Buzz.py
+++ Fizz Buzz.py
@@ -1,11 +1,10 @@
-def fizz_buzz(n):
-    if n % 3 == 0 and n % 5 == 0:
+def fizz_buzz(n,x,y):
+    if n % x == 0 and n % y == 0:
         return("FizzBuzz")
-    elif n % 3 == 0:
+    elif x % 3 == 0:
         return("Fizz")
-    elif n % 5 == 0:
+    elif y % 5 == 0:
         return("Buzz")
     else:
         return(str(n))
     
-N= int(input("自然数を入力してください"))
