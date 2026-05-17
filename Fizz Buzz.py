038dbda8d85244378898f13d598c09263eafbb4f
Noguchi <nogutaku1218@gmail.com>
2026-05-17 16:13:47 +0900

    Fizz Buzz_update

--- Fizz Buzz.py
+++ Fizz Buzz.py
@@ -1,10 +1,9 @@
 def fizz_buzz(n,x,y):
     if n % x == 0 and n % y == 0:
         return("FizzBuzz")
-    elif x % 3 == 0:
+    elif n % x == 0:
         return("Fizz")
-    elif y % 5 == 0:
+    elif n % y == 0:
         return("Buzz")
     else:
         return(str(n))
-    
