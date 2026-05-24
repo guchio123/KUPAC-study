436872eb0beb740a4855b0c22a3b5d1422498a74
Noguchi <nogutaku1218@gmail.com>
2026-05-24 15:02:30 +0900

    スイカ割りゲーム

---
+++ for文.py
@@ -0,0 +1,4 @@
+list_obj = ["apple","orange","banana"]
+for fruit_name in list_obj:
+    upper_name = fruit_name.upper()
+    print(upper_name)
---
+++ 辞書とfor文.py
@@ -0,0 +1,5 @@
+dict_obj = {"dog": "犬", "cat": "猫", "bird": "鳥"}
+
+for english in dict_obj:
+    japanese = dict_obj[english]
+    print(english, "は", japanese, "です")
